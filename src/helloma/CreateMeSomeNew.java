968d77f3bdaa87ec5f53548c93cb92c0b68249fd
Ahmad <Ahmad.Ghafoor@tekschool.us>
2023-02-17 07:05:28 -0500

    this is the first commit for new practice

---
+++ src/helloma/CreateMeSomeNew.java
@@ -0,0 +1,11 @@
+package helloma;
+
+public class CreateMeSomeNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I love selenium");
+	}
+
+}
